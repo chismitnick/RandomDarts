dea915cc863328205ce4baab06698bf3af71ca3d
csamiselo <chisambwe.samiselo@gmail.com>
2020-03-14 17:03:19 +0200

    Progress so far: Basic on Sets and Maps in Dart and usage of key value pairs

---
+++ BasicSet2.dart
@@ -0,0 +1,23 @@
+// When  we  write  the  following,  it  does  not  create  a  Set,  but  a  Map: 
+// i.e var  mylnteger  =  {};
+ // the syntax for Set literals is similar to that of Map literals
+
+
+ // the literal {} is default to the Map type
+
+ //in  general,  a  map  is  an  object 
+// that  associates  keys  with  values.  
+//The  set  has  also  keys,  but  they  are  implicit.
+
+
+main(List<String>arguments){
+
+var myInteger = {};
+
+if(myInteger.isEmpty){
+
+print("The map is empty and has no key value pairs ");
+}
+
+
+}
---
+++ BasicSet3.dart
@@ -0,0 +1,26 @@
+/*
+Smple example  of  the  Map  type  by  mapping  literals.  
+While  writing  keys  and  values,  it  is  important  to  note  that  
+each  key  occurs  only  once,  but 
+you  can  use  the  same  value  many times 
+*/
+
+main(List<String>arguments){
+
+
+var  myProducts  =  {
+ 'first'  :  'TV',
+ 'second'  :  'Refrigerator',
+ 'third'  :  'Mobile',
+ 'fourth'  :  'Tablet',
+ 'fifth'  :  'Computer',
+ 'price'  : '150'
+ };
+ print('The name of the products is');
+ print(myProducts['third']);
+
+ print('The price of the product is :');
+ print(myProducts['price']);
+
+
+}
