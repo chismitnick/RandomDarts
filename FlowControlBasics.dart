428389c410a581cec59a9bc1e718524a8877e1a3
csamiselo <chisambwe.samiselo@gmail.com>
2020-03-18 15:55:16 +0200

    Progress: Conditiona expressions in Dart

---
+++ FlowControlBasics.dart
@@ -0,0 +1,32 @@
+// Demonstration of Flow controol using if-else statements in dart
+
+main(List<String> arguments) {
+  bool first = true;
+  bool second = false;
+  bool third = true;
+  bool fourth = false;
+
+  if (first)
+    print('The first gear is engaged');
+  else
+    print('Engage the second gear');
+
+//Block style if-else
+  if (second) {
+    print('Secong gear is engaged');
+  } else {
+    print('the Second gear is not engaged');
+  }
+
+  if (third) {
+    print('Third gear is engaged ');
+  } else {
+    print('The third gear is not engaged');
+  }
+
+  if (fourth) {
+    print('The fourth gear has been engaged');
+  } else {
+    print('The fourth gear is not engaged');
+  }
+}
